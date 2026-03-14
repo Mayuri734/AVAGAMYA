e6576d9942574c64f940f43ef6932d3d8256bb05
Mayuri734 <anandikarmayuri@gmail.com>
2026-03-14 17:30:35 +0530

    feat: Add Playwright UI test for the FAQs page.

--- tests/frontend/ui_faqs.spec.ts
+++ tests/frontend/ui_faqs.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://avagamya.vercel.app/');
   await page.getByRole('link', { name: 'FAQs' }).click();
-  await expect(page.locator('section')).toBeVisible();
+  await expect(page.locator('section').first()).toBeVisible();
   await page.getByRole('button', { name: 'Is my personal data safe?' }).click();
   await page.getByRole('button', { name: 'What types of documents can I' }).click();
   await page.getByRole('button', { name: 'Contact Support →' }).click();
